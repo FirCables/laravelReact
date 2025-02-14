c7acc174b32144501ffc235fd69426e5fedcdbc5
FirCables <leleregina14@gmail.com>
2025-02-14 08:06:06 +0100

    aghhhhg pluhh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,14 +19,24 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $categories = [
-          
-                ['name' => 'alkatresz'],
-                ['name' => 'külső kiegészítő'],
-                ['name' => 'belső kiegészítő']
+
+            ['name' => 'alkatresz'],
+            ['name' => 'külső kiegészítő'],
+            ['name' => 'belső kiegészítő']
         ];
-            foreach ($categories as $category) {
-                Category::create($category);
-        
-    }
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+
+            // JSON fájl beolvasása
+            $json = File::get(database_path('data/products.json'));
+
+            // JSON dekódolása tömbbé
+            $products = json_decode($json, true);
+   
+            // Adatok beszúrása az adatbázisba
+            foreach ($products as $product) {
+                Product::create($product);
+            }
     }
 }
